a03987c06f27e3ab64d48ca0ab280faa33ecb836
Lizdigital <lizetteclaireh@gmail.com>
2024-06-26 16:51:28 +0100

    Update project tabs to change background colour when clicked on

--- JS/tabs.js
+++ JS/tabs.js
@@ -1,14 +1,62 @@
-const links = document.querySelectorAll('.Tabs');
+// const links = document.querySelectorAll('.Tabs');
 
-links.forEach(link => {
-    link.addEventListener('click', function (event) {
+
+// const tab = document.getElementsByClassName('tab');
+
+
+// links.forEach(link => {
+//     link.addEventListener('click', function (event) {
         // Prevent the default link behavior (e.g., navigating to the top of the page)
-        event.preventDefault();
+        // event.preventDefault();
 
         // Remove "active" class from all links
-        links.forEach(link => link.classList.remove('active'));
+        // links.forEach(link => link.classList.remove('active'));
 
         // Add "active" class to the clicked link
-        this.classList.add('active');
+//         this.classList.add('active');
+//     });
+// });
+
+
+// for (let tab of links) {
+//     tab.addEventListener('click', function(e) {
+//         console.log('clicked');
+//         e.preventDefault();
+//         console.log(e);
+//         let selected = e.target;
+
+//         selected.toggle.style.background = '#3456cb';
+//         selected.classList.add('active');
+//         selected = 'null';
+// })};
+
+
+// links.forEach(link =>{
+//     link.addEventListener('click', function(e){
+//         e.preventDefault();
+//         let selected = e.target;
+//         console.log(selected);
+//         if (selected) {
+//             selected === ':first-child';
+//         }
+
+//     })})
+
+const links = document.querySelectorAll('.Tabs');
+const content = document.querySelectorAll('.content');
+
+// Attach click event listener to each link element
+links.forEach((link, index) => {
+    link.addEventListener('click', function() {
+        // Remove 'active' class from all links
+        links.forEach(link => {link.classList.remove('active')});
+
+        // Add 'active' class to the clicked link
+        link.classList.add('active');
+
+        content.forEach(content => {content.classList.remove('active')});
+        content[index].classList.add('active');
     });
+
+
 });
